2f2ebfc13f3ba58e026d974a477d5701ff26d57f
JuliaValerio <38157485+JuliaValerio@users.noreply.github.com>
2023-05-24 17:37:22 -0300

    Update ListaTema.tsx

--- src/components/temas/listatema/ListaTema.tsx
+++ src/components/temas/listatema/ListaTema.tsx
@@ -12,6 +12,7 @@ import './ListaTemas.css'
 
 function ListaTema() {
   const [temas, setTemas] = useState<Tema[]>([])
+   const [token, setToken] = useLocalStorage('token');
   let navigate = useNavigate();
   const token = useSelector<TokenState, TokenState["tokens"]>(
     (state) => state.tokens
